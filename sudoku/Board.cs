112bba7e8ffe74a49a2ac3dde9fddb69ab04dabf
zeynep esen <zfm@anadolu.edu.tr>
2016-02-25 22:06:29 +0200

    ilk commit

---
+++ sudoku/Board.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudoku
+{
+    class Board
+    {
+        public Cell[,] Table = new Cell[9, 9];
+        public State State { get; set; }
+
+        public bool IsValid()
+        {
+            Cell[] row = new Cell[9];
+
+            // satır
+            for (var rowIndex = 0; rowIndex < 9; rowIndex++)
+            {
+                for (var colIndex = 0; colIndex < 9; colIndex++)
+                {
+                    row[colIndex] = Table[rowIndex, colIndex];
+                }
+                if (!ArraySayiKontrol(row))
+                {
+                    return false;
+                }
+            }
+            // sütün
+            for (var colIndex = 0; colIndex < 9; colIndex++)
+            {
+               // Cell[] col = new Cell[9];
+                for (var rowIndex = 0; rowIndex < 9; rowIndex++)
+                {
+                    row[rowIndex] = Table[rowIndex, colIndex];
+                }
+                if (!ArraySayiKontrol(row))
+                {
+                    return false;
+                }
+            }
+            // kare
+            for (var index = 0; index < 9; index = index + 3)
+            {
+                
+                for (var y = 0; y < 9; y = y + 3)
+                {
+                   // Cell[] sqr = new Cell[9];
+                    var rowIndex = 0;
+                    for (var i = index; i < index + 3; i++)
+                    {
+                        for (var k = y; k < y + 3; k++)
+                        {
+                            row[rowIndex++] = Table[i, k];
+                        }
+                    }
+                    if (!ArraySayiKontrol(row))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private bool ArraySayiKontrol(Cell[] cells)
+        {
+            bool[] numbers = new bool[9];
+            //for (var index = 0; index < numbers.Length; index++)
+            //{
+            //    numbers[index] = false;
+            //}
+            foreach (Cell cell in cells)
+            {
+                if (cell.value == 0)
+                {
+                    continue;
+                }
+                bool value = numbers[cell.value - 1];
+                if (value)
+                {
+                    return false;
+                }
+                numbers[cell.value- 1] = true;
+            }
+            return true;
+        }
+    }
+}
---
+++ sudoku/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace sudoku
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dosya_yukle_Click(object sender, EventArgs e)
+        {
+            Board board = new Board();
+           
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = @"C:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    
+                        using (StreamReader sr = new StreamReader(theDialog.FileName))
+                        {
+                            int y = 1;
+                            int separatrixY = 1;
+                            string numbers;
+                            int row = 0;
+                            while ((numbers = sr.ReadLine()) != null)
+                            {
+                                int x = 1;
+                                int separatrixX = 0;
+                                for (int i = 0; i < numbers.Length; i++)
+                                {
+                                    byte number = byte.Parse(numbers[i].ToString());
+                                    if (separatrixX % 3 == 0)
+                                    {
+                                        x = x + 5;
+                                    }
+
+                                    TextBox tb = new TextBox();
+                                    tb.Size = new System.Drawing.Size(15, 15);
+                                    tb.TextAlign = contentAlign;
+                                    if (number == 0)
+                                    {
+                                        tb.Text = " ";
+                                        board.Table[row, i] = new Cell { value = 0 };
+                                    }
+                                    else
+                                    {
+                                        tb.Text = number.ToString();
+                                        board.Table[row, i] = new Cell { value = number };
+                                    }
+                                    Point p = new Point(50 + x, 50 + y);
+                                    tb.Location = p;
+                                    this.Controls.Add(tb);
+                                    separatrixX++;
+                                    x = x + 15;
+                                }
+
+                                if (separatrixY % 3 == 0)
+                                {
+                                    y = y + 25;
+                                }
+                                else
+                                {
+                                    y = y + 20;
+                                }
+
+                                separatrixY++;
+                                row++;
+                            }
+                        }
+                    //board kontrol
+                    bool valid = board.IsValid();
+                    Console.WriteLine(valid);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+
+        public HorizontalAlignment contentAlign { get; set; }
+
+        public string lines { get; set; }
+    }
+}
+////listBox1.Items.Clear();
+
+//            // Dosyamızı okuyacak.
+//            StreamReader oku;
+
+//            // Belirtmiş olduğum yoldaki dosyayı açacak. 
+//            /* NOT: @ bu işareti koymamın nedeni \\ 2 defa bundan 
+//            yapmamak içindir. */
+//            oku = File.OpenText(@"C:\Documents and Settings\YIGIT
+//            \Belgelerim\visual studio 2010
+//            \Projects\TxtVeriOkuma\TxtVeriOkuma\bin\Debug\C#.txt");
+
+//            string yazi;
+
+//            // Satır boş olana kadar okumaya devam eder.
+//            while ((yazi = oku.ReadLine()) != null)
+//            {
+//                // Listbox'ı .txt içeriği ile doldur.
+//                lstOku.Items.Add(yazi.ToString());
+//            }
