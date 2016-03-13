112ec374da39de15662f705c2ca9fa645ac8b842
zfme <zeynepfeyzamirza@gmail.com>
2016-03-13 14:56:49 +0200

    dfssolver sınıfıda eklendi

---
+++ sudoku/DFSSolver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudoku
+{
+    public class DFSSolver
+    {
+        //List<Queue<Board>> queue;
+        private Stack<Board> stack=new Stack<Board>(); 
+        public Board SolveWithDFS(Board board)
+        {
+            //bool solved = false;
+            stack.Push(board);
+            while (stack.Count>0)
+            {
+                Board current = stack.Pop();
+                current.FillPossibleValues();
+                if (current.IsSolved())
+                {
+                    return current;
+                }
+                current.FillStackWithFirstPossibleValues(stack);
+            }
+            return null;
+        }
+    }
+}
