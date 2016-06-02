111ab3233e1cf1f7d2df3154bffb39fb8d81e4cb
zeynep esen <zfm@anadolu.edu.tr>
2016-06-02 21:49:18 +0300

    peek queue boşsa exception handle edildi

--- sudoku/LoQSolver.cs
+++ sudoku/LoQSolver.cs
@@ -22,95 +22,101 @@ namespace sudoku
             // ilk queue ya board u ekle
             queueList[0].Enqueue(new BoardDFSQ { Board = board, State = State.Empty });
             // çözüm objesi
-            int maxChildCount = 4;
+            int maxChildCount = 6;
             bool cozuldu = false;
             Board cozumBoard = null;
             var options = new ParallelOptions { MaxDegreeOfParallelism = 8 };
-            Parallel.For(0, 1, options, (i, loopState) =>
+            Parallel.For(0, 4, options, (i, loopState) =>
             {
 
-              //  Console.WriteLine("Started thread={0}, i={1}", Thread.CurrentThread.ManagedThreadId, i); 
+                //  Console.WriteLine("Started thread={0}, i={1}", Thread.CurrentThread.ManagedThreadId, i); 
                 while (!cozuldu)
                 {
-                for (int q = 64; q >= 0; q--)
-                {
-                    if (cozuldu)
+                    for (int q = 64; q >= 0; q--)
                     {
-                        break;
-                    }
-                    BoardDFSQ boardDfsq;
-                    if (queueList[q].Count == 0)
-                    {
-                        continue;
-                    }
-                    bool locked = false;
-                    Monitor.TryEnter(boardlocks[q], ref locked);
+                        if (cozuldu)
+                        {
+                            break;
+                        }
+                        BoardDFSQ boardDfsq;
+                        if (queueList[q].Count == 0)
+                        {
+                            continue;
+                        }
+                        bool locked = false;
+                        Monitor.TryEnter(boardlocks[q], ref locked);
                         if (!locked)
                         {
                             i = 65;
                             continue;
                         }
-                    
 
-                        boardDfsq = queueList[q].Peek();
-
+                        try
+                        {
+                            boardDfsq = queueList[q].Peek();
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            continue;
+                        }
                         if (boardDfsq == null)
                         {
                             continue;
                         }
-                            if (boardDfsq.State.Equals(State.Empty))
-                            {
-                                boardDfsq.State = State.Processing;
-                            }
-                            else if (boardDfsq.State == State.Processing) {
-                                break;
-                            }               
-                                if (boardDfsq.Board.IsSolved()) 
-                                {
-                                    cozuldu = true;
-                                    cozumBoard = boardDfsq.Board;
-                                    break;
-                                }
+                        if (boardDfsq.State.Equals(State.Empty))
+                        {
+                            boardDfsq.State = State.Processing;
+                        }
+                        else if (boardDfsq.State == State.Processing)
+                        {
+                            break;
+                        }
+                        if (boardDfsq.Board.IsSolved())
+                        {
+                            cozuldu = true;
+                            cozumBoard = boardDfsq.Board;
+                            break;
+                        }
                         int childCount = 0;
                         for (var index = 0; index < 9; index++)
                         {
-                            for (var indexy = 0; indexy < 9 && childCount<maxChildCount; indexy++)
+                            for (var indexy = 0; indexy < 9 && childCount < maxChildCount; indexy++)
                             {
                                 if (boardDfsq.Board.Table[index, indexy].Value == 0)
-                                {                                  
-                                        for (int k = 0; k < boardDfsq.Board.Table[index,indexy].PossibleValues.Count; k++)
+                                {
+                                    for (int k = 0; k < boardDfsq.Board.Table[index, indexy].PossibleValues.Count; k++)
+                                    {
+                                        byte possibleValue = boardDfsq.Board.Table[index, indexy].PossibleValues[k];
+                                        boardDfsq.Board.Table[index, indexy].PossibleValues.RemoveAt(k);
+                                        k--;
+                                        BoardDFSQ child = boardDfsq.Copy();
+                                        child.Board.Table[index, indexy].Value = possibleValue;
+                                        child.Board.FillPossibleValues();
+                                        if (child.Board.IsValidDfsq())
                                         {
-                                            byte possibleValue = boardDfsq.Board.Table[index, indexy].PossibleValues[k];
-                                            boardDfsq.Board.Table[index, indexy].PossibleValues.RemoveAt(k);
-                                            k--;
-                                            BoardDFSQ child = boardDfsq.Copy();
-                                            child.Board.Table[index, indexy].Value = possibleValue;
-                                            child.Board.FillPossibleValues();
-                                            if (child.Board.IsValidDfsq())
+                                            child.State = State.Empty;
+                                            child.QueueNdx = q + 1;
+                                            queueList[q + 1].Enqueue(child);
+                                            childCount++;
+                                            if (childCount == maxChildCount)
                                             {
-                                                child.State = State.Empty;
-                                                child.QueueNdx = q + 1;
-                                                queueList[q + 1].Enqueue(child);
-                                                childCount++;
-                                                if (childCount == maxChildCount)
-                                                {
-                                                    break;
-                                                }
+                                                break;
                                             }
+                                        }
 
                                     }
-                                  
+
                                 }
                             }
                         }
 
                         queueList[q].Dequeue();
                         Monitor.Exit(boardlocks[q]);
-                        
-                     
-                        
-                   //     Console.WriteLine("Queue check peek-{0}:{1} thread={2}, i={3}", q, boardDfsq,
-                     //       Thread.CurrentThread.ManagedThreadId, i);
+
+
+
+                        //     Console.WriteLine("Queue check peek-{0}:{1} thread={2}, i={3}", q, boardDfsq,
+                        //       Thread.CurrentThread.ManagedThreadId, i);
                     }
                 }
             });
